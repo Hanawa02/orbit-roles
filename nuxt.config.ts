9fa9967651b1cabcaab3f23ebc165751f74f1a4b
hanawa02 <laura@laura-melo.com>
2024-12-16 15:20:31 -0300

    chore: setup supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase']
-})
+  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  supabase: {
+    redirectOptions: {
+      login: "/auth/login",
+      callback: "/auth/confirm",
+      include: undefined,
+      exclude: ["/auth/login", "/auth/signup"],
+      cookieRedirect: true,
+    },
+    cookieName: process.env.SUPABASE_COOKIE_NAME,
+  },
+  runtimeConfig: {
+    public: {
+      supabase: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_KEY,
+        cookieName: process.env.SUPABASE_COOKIE_NAME,
+      },
+    },
+  },
+});
