28ddc35787d90bb84a422ed3d5c16e3bb60cfaf4
hanawa02 <laura@laura-melo.com>
2024-12-21 00:10:31 -0300

    chore: temp logout button

--- pages/index.vue
+++ pages/index.vue
@@ -3,4 +3,22 @@
     <h1>Orbit Roles</h1>
     <p>Your rotating roles management tool!</p>
   </section>
+  <section>Hello, {{ user?.user_metadata.displayName }}</section>
+
+  <button @click="logout">Logout</button>
 </template>
+
+<script setup lang="ts">
+const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const logout = async () => {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error(error);
+    return;
+  }
+  navigateTo("/auth/login");
+};
+</script>
