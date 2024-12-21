79d84b19cecdffe24d6df7bb8c84a4c39a8206c6
hanawa02 <laura@laura-melo.com>
2024-12-21 11:38:59 -0300

    feat: use translations for login form

--- components/auth/LoginForm.vue
+++ components/auth/LoginForm.vue
@@ -2,11 +2,11 @@
   <form @submit="onSubmit">
     <FormField v-slot="{ componentField }" name="email">
       <FormItem>
-        <FormLabel>E-mail</FormLabel>
+        <FormLabel>{{ login_form_email_label() }}</FormLabel>
         <FormControl>
           <Input
             type="text"
-            placeholder="ada.lovelace@gmail.com"
+            :placeholder="login_form_email_placeholder()"
             v-bind="componentField"
           />
         </FormControl>
@@ -15,24 +15,33 @@
     </FormField>
     <FormField v-slot="{ componentField }" name="password">
       <FormItem>
-        <FormLabel>Password</FormLabel>
+        <FormLabel>{{ login_form_password_label() }}</FormLabel>
         <FormControl>
           <Input
             type="password"
-            placeholder="The one you used to sign up."
+            :placeholder="login_form_password_placeholder()"
             v-bind="componentField"
           />
         </FormControl>
         <FormMessage />
       </FormItem>
     </FormField>
-    <Button type="submit" class="mt-6">Login</Button>
+    <Button type="submit" class="mt-6">{{ login_form_button() }}</Button>
   </form>
 </template>
 
 <script setup lang="ts">
 import { useForm } from "vee-validate";
 import { toTypedSchema } from "@vee-validate/zod";
+
+import {
+  login_form_email_label,
+  login_form_email_placeholder,
+  login_form_password_label,
+  login_form_password_placeholder,
+  login_form_button,
+} from "translations";
+
 import * as z from "zod";
 
 import { Button } from "~/components/ui/button";
